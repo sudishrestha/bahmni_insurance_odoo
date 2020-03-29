457ab8115739d615925e031c76189520841fc9aa
Sudish <sudishrestha@gmail.com>
2020-03-29 15:28:05 +0545

    Added IPD package module

--- models/insurance_disease_code.py
+++ models/insurance_disease_code.py
@@ -4,10 +4,10 @@ import json
 
 _logger = logging.getLogger(__name__)
 class insurance_disease_code(models.Model):
-    _name = 'insurance.odoo.disease.code'
+    _name = 'insurance.odoo.ipd.code'
     
     ipdCode = fields.Char(string="IPD Code", help="This field is used to code of the disease as in IPD", required=True)
-    disease = fields.Char(string="Disease", help="This field is used to store disease", required=True)
+    diagnosis = fields.Char(string="Diagnosis", help="This field is used to store disease", required=True)
     # insurance_price = fields.Float(string="Insurance Product Price", help="This field is used to store Insurance product price in Insurance System", required=True)
     # odoo_product_id = fields.Many2one('product.product', string="Odoo Product")
     # valid_from = fields.Datetime(string="Valid From")
